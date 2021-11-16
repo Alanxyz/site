536b199d9601acfd9f467f601adc37d3e7e912ae
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-11-16 08:48:57 -0300

    Delete script.js

--- projects/script.js
+++
@@ -1,7 +0,0 @@
-function show(){
-    let html = document.getElementById('html');
-    let css = document.getElementById('css');
-    let js = document.getElementById('js')
-
-    document.getElementById('iframe').setAttribute("srcdoc", `${html.value} <style>${css.value}</style> <script>${js.value}</script>`);
-}
