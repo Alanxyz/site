b79fc5384f007e7a282cd1f6701de87cd2329661
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-11-27 19:00:35 -0300

    Create home.js

---
+++ code/home.js
@@ -0,0 +1,8 @@
+const blogs_element = document.getElementById('blogs');
+
+fetch('/json/blogs.json')
+    .then(response => response.json())
+    .then(data => data.blogs.map(blog => {
+        blogs_element.innerHTML += `<br><a href="${blog.link}">${blog.title}</a>`;
+        console.log(blog.link)
+    }));
