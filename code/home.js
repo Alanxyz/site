c69f2a6b91352bc7b7efd6b6476ad3dd1d118e6f
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-11-27 19:05:29 -0300

    Update home.js

--- code/home.js
+++ code/home.js
@@ -1,6 +1,6 @@
 const blogs_element = document.getElementById('blogs');
 
-fetch('/json/blogs.json')
+fetch('../json/blogs.json')
     .then(response => response.json())
     .then(data => data.blogs.map(blog => {
         blogs_element.innerHTML += `<br><a href="${blog.link}">${blog.title}</a>`;
