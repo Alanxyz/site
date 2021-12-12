8f1f3cb0b67653d31808ae40a327919c198319d4
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-12-11 23:26:42 -0300

    Update home.js

--- code/home.js
+++ code/home.js
@@ -1,7 +1,11 @@
-const blogs_element = document.getElementById('blogs');
+// Define all
+const date = new Date();
 
-fetch('/json/blogs.json')
-    .then(response => response.json())
-    .then(data => data.blogs.map(blog => {
-        blogs_element.innerHTML += `<br><a href="${blog.link}">${blog.title}</a>`;
-    }));
+// Festivity Update
+if(date.getMonth() == 11){
+    document.title = document.title + ' | Christmas'
+} else if(date.getMonth() == 9) {
+    document.title = document.title + ' | Halloween'
+} else if(date.getMonth() == 11 && date.getDay() == 30){
+    document.title = document.title + ' | Happy New Year'
+}
