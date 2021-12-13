ea56e5b20791ded7b6f451eae54e4b36c7e71335
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-12-13 16:08:43 -0300

    Update home.js

--- code/home.js
+++ code/home.js
@@ -9,3 +9,21 @@ if(date.getMonth() == 11){
 } else if(date.getMonth() == 11 && date.getDay() == 30){
     document.title = document.title + ' | Happy New Year'
 }
+
+// Function
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction() {
+    var button = document.getElementById('upbutton');
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function up(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
