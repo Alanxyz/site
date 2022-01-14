757659bdbe1db51df06f09dd1ab6d2000f1d5eb1
ZSendokame <sendokame@gmail.com>
2022-01-14 20:03:05 -0300

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,12 +1,23 @@
-window.onscroll = function(){scrollFunction()};
+// Define all
+const date = new Date();
 
-function scrollFunction() {
+// Festivity Update
+if(date.getMonth() == 11){
+    document.title = `🎄${document.title}🎄`
+} else if(date.getMonth() == 9) {
+    document.title = `🎃${document.title}🎃`
+} else if(date.getMonth() == 11 && date.getDay() == 30){
+    document.title = `🎉${document.title}🎉`
+}
+
+// Function
+window.onscroll = () => {
     var button = document.getElementById('upbutton');
-
+    
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        button.style.display = "block";
+            button.style.display = "block";
     } else {
-        button.style.display = "none";
+            button.style.display = "none";
     }
 }
 
